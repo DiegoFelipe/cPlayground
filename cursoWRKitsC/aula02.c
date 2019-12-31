5f671f527e7cf0f4f640c32ec17ca1d04decd958
Diego Felipe <diegofelipe.14@gmail.com>
2019-12-31 16:35:25 -0400

    Aula 02

---
+++ cursoWRKitsC/aula02.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main ()
+{
+    int numero;
+    printf("Digite um numero: \n");
+    scanf("%d", &numero);
+    getchar();
+    printf("Número digitado %d \n", numero);
+    return 0;
+
+}
